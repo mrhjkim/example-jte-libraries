baf6263258f8c9202112bd68c2ac7e3785dad2d6
Kim HaeJoo <mrhjkim@uangel.com>
2020-02-04 14:12:40 +0900

    add go compile env

--- make/build.groovy
+++ make/build.groovy
@@ -1,8 +1,11 @@
 void call(){
-  node {
-	  stage("Build"){
-		println "make: build()"
-		sh 'make build'
-	  }
-  }
+	node {
+		def root = tool name: 'Go 1.13', type: 'go'
+		withEnv(["GOROOT=${root}", "PATH+GO=${root}/bin:${HOME}/go/bin"]) {	
+			stage("Build"){
+				println "make: build()"
+					sh 'make build'
+			}
+		}
+	}
 }
--- make/unit_test.groovy
+++ make/unit_test.groovy
@@ -1,8 +1,11 @@
 void call(){
-  node {
-	  stage("Unittest"){
-		println "make: unit_test()"
-		sh 'make junittest'
-	  }
-  }
+	node {
+		def root = tool name: 'Go 1.13', type: 'go'
+		withEnv(["GOROOT=${root}", "PATH+GO=${root}/bin:${HOME}/go/bin"]) {	
+			stage("Unittest"){
+				println "make: unit_test()"
+					sh 'make junittest'
+			}
+		}
+	}
 }
